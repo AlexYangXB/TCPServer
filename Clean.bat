52010127fa9995e2e94f86773e0bfaeb6d8837cf
davidhxy678 <2623000520@qq.com>
2015-06-16 10:40:41 +0800

    clean.bat

--- Clean.bat
+++ Clean.bat
@@ -1,4 +1,9 @@
 set curdir=%~dp0\
 del /a /f /s /q "%curdir%*.FSN"
 del /a /f /s /q "%curdir%*.log"
+del /a /f /s /q "%curdir%MaterialSkin\bin\*.dll"
+del /a /f /s /q "%curdir%ServerForm\bin\*.dll"
+del /a /f /s /q "%curdir%TcpServer\bin\*.dll"
+del /a /f /s /q "%curdir%KyBll\bin\Debug\*.dll"
+del /a /f /s /q "%curdir%KyModel\bin\*.dll"
 pause
