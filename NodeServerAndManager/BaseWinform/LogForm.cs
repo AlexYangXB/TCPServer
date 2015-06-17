fd694e57329572e64fecd843e69d5a170557b768
davidhxy678 <2623000520@qq.com>
2015-06-17 15:18:37 +0800

    日志显示跨线程委托

--- NodeServerAndManager/BaseWinform/LogForm.cs
+++ NodeServerAndManager/BaseWinform/LogForm.cs
@@ -10,54 +10,72 @@ namespace KangYiCollection.BaseWinform
         public LogForm()
         {
             InitializeComponent();
-            
+
         }
         public void myTcpServer_LogEvent(object sender, MyTCP.LogEventArgs e)
         {
-            try
+            AddLog(e);
+
+        }
+        private delegate void AddStatusDelegate(MyTCP.LogEventArgs e);
+        /// <summary>
+        /// 添加日志
+        /// </summary>
+        /// <param name="e"></param>
+        private void AddLog(MyTCP.LogEventArgs e)
+        {
+            if (this.InvokeRequired)
             {
-                if (CommandLogRichTextBox.Lines.Length > 200)
+                AddStatusDelegate d = new AddStatusDelegate(AddLog);
+                this.Invoke(d, new object[] { e });
+            }
+            else
+            {
+                try
                 {
-                    CommandLogRichTextBox.Clear();
+                    //命令日志
+                    if (CommandLogRichTextBox.Lines.Length > 200)
+                    {
+                        CommandLogRichTextBox.Clear();
+                    }
+                    if (e.Type == LogType.Command)
+                    {
+                        CommandLogRichTextBox.AppendText(e.Message);
+                        CommandLogRichTextBox.Focus();
+                        CommandLogRichTextBox.Select(CommandLogRichTextBox.TextLength, 0);
+                        CommandLogRichTextBox.ScrollToCaret();
+                    }
+                    //业务日志
+                    if (BussinessLogRichTextBox.Lines.Length > 200)
+                    {
+                        BussinessLogRichTextBox.Clear();
+                    }
+                    if (e.Type == LogType.Bussiness)
+                    {
+                        BussinessLogRichTextBox.AppendText(e.Message);
+                        BussinessLogRichTextBox.Focus();
+                        BussinessLogRichTextBox.Select(BussinessLogRichTextBox.TextLength, 0);
+                        BussinessLogRichTextBox.ScrollToCaret();
+                    }
+                    //FSN导入日志
+                    if (FSNImortLogRichTextBox.Lines.Length > 200)
+                    {
+                        FSNImortLogRichTextBox.Clear();
+                    }
+                    if (e.Type == LogType.FSNImport)
+                    {
+                        FSNImortLogRichTextBox.AppendText(e.Message);
+                        FSNImortLogRichTextBox.Focus();
+                        FSNImortLogRichTextBox.Select(FSNImortLogRichTextBox.TextLength, 0);
+                        FSNImortLogRichTextBox.ScrollToCaret();
+                    }
 
                 }
-                if (BussinessLogRichTextBox.Lines.Length > 200)
+                catch (Exception ex)
                 {
-                    BussinessLogRichTextBox.Clear();
-
-                }
-                if (FSNImortLogRichTextBox.Lines.Length > 200)
-                {
-                    FSNImortLogRichTextBox.Clear();
-
-                }
-                if (e.Type==LogType.Command)
-                {
-                    CommandLogRichTextBox.AppendText(e.Message);
-                    CommandLogRichTextBox.Focus();
-                    CommandLogRichTextBox.Select(CommandLogRichTextBox.TextLength, 0);
-                    CommandLogRichTextBox.ScrollToCaret();
-                }
-                if (e.Type == LogType.Bussiness)
-                {
-                    BussinessLogRichTextBox.AppendText(e.Message);
-                    BussinessLogRichTextBox.Focus();
-                    BussinessLogRichTextBox.Select(BussinessLogRichTextBox.TextLength, 0);
-                    BussinessLogRichTextBox.ScrollToCaret();
-                }
-                if (e.Type == LogType.FSNImport)
-                {
-                    FSNImortLogRichTextBox.AppendText(e.Message);
-                    FSNImortLogRichTextBox.Focus();
-                    FSNImortLogRichTextBox.Select(FSNImortLogRichTextBox.TextLength, 0);
-                    FSNImortLogRichTextBox.ScrollToCaret();
+                    Log.ConnectionException("日志输出异常!", ex);
                 }
             }
-            catch (Exception ex)
-            {
-                Log.ConnectionException("日志输出异常!",ex);
-            }
-            
         }
 
     }
